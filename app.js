c8c52afd54b415342ac2d1eab3a8564c2cc82c45
Connor Bode <connor.bode@gmail.com>
2015-04-28 21:33:30 -0400

    running :)

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-var Twitter = require('twitter');
+var Twitter     = require('twitter');
+var MongoClient = require('mongodb').MongoClient;
+var logger      = require('tracer').colorConsole();
+var t           = require('es6-template-strings');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
@@ -7,8 +10,85 @@ var client = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
 });
 
-client.stream('statuses/filter', {track: 'i wish'}, function (stream) {
-  stream.on('data', function (tweet) {
-    console.log(tweet.text);
-  });
-});
+var collection;         // the mongodb collection of tweets
+var docsAdded   = 0;    // the number of documents added to mongo
+var buffer      = [];   // the buffer of tweets waiting to be added
+var bufferLimit = 100;  // the buffer size which triggers a dump to mongo
+
+//
+// onBufferInserted
+// ----------------
+//
+// runs when the `dumpBuffer` method
+// returns. verifies success of method.
+//
+var onBufferInserted = function (err, status) {
+  if (err) return logger.error(err);
+  docsAdded += status.result.n;
+  var message = t('${r} total tweets dumped to db!', { r: docsAdded });
+  logger.info(message);
+};
+
+//
+// dumpBuffer
+// ----------
+//
+// dumps the buffer to mongodb and resets
+// the buffer.
+// 
+var dumpBuffer = function () {
+  logger.info('dumping buffer');
+  collection.insert(buffer, onBufferInserted);
+  buffer = [];
+};
+
+//
+// onTweet
+// -------
+//
+// runs every time a tweet is received from
+// the twitter stream.  adds the tweet to
+// the buffer.
+// 
+var onTweet = function (tweet) {
+  var tweetOnly = {
+    tweet: tweet.text
+  };
+  buffer.push(tweetOnly);
+  if (buffer.length > bufferLimit)
+    dumpBuffer();
+};
+
+// 
+// onTwitterConnected
+// ------------------
+// 
+// runs when the twitter stream
+// has been initialized
+// 
+var onTwitterConnected = function (stream) {
+  logger.info('twitter stream established');
+  stream.on('data', onTweet);
+};
+
+//
+// onMongoConnected
+// ----------------
+// 
+// runs after the application has established
+// a connection with mongodb.  
+//
+// - gets the collection of tweets
+// - initializes a twitter stream
+// 
+var onMongoConnected = function (err, db) {
+  if (err) return logger.error(err);
+  logger.info('connected to mongodb');
+
+  collection = db.collection('tweets');
+  client.stream('statuses/filter', {track: 'i wish'}, onTwitterConnected);
+};
+
+
+// run!
+MongoClient.connect(process.env.MONGO_URL, onMongoConnected);
